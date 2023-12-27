4a120819a3ca65f3208a4807b0c4317954d10226
athira <athiraos1997@gmai.com>
2023-12-27 18:25:55 +0530

    changes

--- src/Compoents/ChildComponents/Sunheader.js
+++ src/Compoents/ChildComponents/Sunheader.js
@@ -3,7 +3,11 @@ import styles from '../../Css/sunheader.module.css';
 function Sunheader() {
     return (
       <div className={styles.sunheader}>
-         Sunheader 
+         <div className={styles.left}>
+          <p className={styles.dash}>Dashboard</p>
+          <button className={styles.addNew}><span  className={styles.addNewText}>Add new</span></button>
+         </div>
+         <div className={styles.right}></div>
        </div>
     );
   }
